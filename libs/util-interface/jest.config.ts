8212c7f94c402c51656b2bfaab8c5d8c01d49825
tyler parker <tparker42d2@gmail.com>
2023-02-16 13:15:26 -0700

    End of Lab-9

--- libs/util-interface/jest.config.ts
+++ libs/util-interface/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'api-util-interface',
-  preset: '../../../jest.preset.js',
+  displayName: 'util-interface',
+  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/api/util-interface',
+  coverageDirectory: '../../coverage/libs/util-interface',
 };
