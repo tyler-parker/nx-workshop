fbbfeb663bee41ba87d1730962d677bed24dede1
tyler parker <tparker42d2@gmail.com>
2023-02-17 12:29:58 -0700

    Migrated up to end of lab 16

--- apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
+++ apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
@@ -1,11 +1,8 @@
 describe('store-ui-shared', () => {
   beforeEach(() =>
-    cy.visit('/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;')
+    cy.visit('/iframe.html?id=headercomponent--primary&args=title;')
   );
   it('should render the component', () => {
     cy.get('bg-hoard-header').should('exist');
-  });
-  it('should show the title', () => {
-    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
